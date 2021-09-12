a9a67d16eacb66aa792dab18338b4d1b1ffa16b6
pedeEli <74102959+pedeEli@users.noreply.github.com>
2021-09-12 04:11:57 +0200

    Changed window icon

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, '..', 'public', 'favicon.png')
   });
 
   mainWindow.maximize();
